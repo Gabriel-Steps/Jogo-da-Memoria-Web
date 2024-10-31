18929750cda9b4242e90df3c2c974ae39d9ff65e
Gabriel Passos Francisco <gabrielpassosfrancisco@gmail.com>
2024-10-31 15:59:42 -0300

    Projeto Final

---
+++ README.md
@@ -0,0 +1,15 @@
+<img src="./src/imgs/image.png">
+
+<h1>Jogo da Memória</h1>
+
+<p>O projeto é o "<b>Jogo da Memória</b>", bem simples... o projeto foi realizado usando <b>HTML, CSS e Javascrpt</b>.
+
+<h1>Tecnologias usadas</h1>
+<ul>
+    <li><h3>HTML</h3></li>
+    <li><h3>CSS</h3></li>
+    <li><h3>Javascript</h3></li>
+</ul>
+
+<h1>Mais sobre o projeto</h1>
+<p>O projeto foi realizado pela <a href="https://github.com/digitalinnovationone" target="_blank">DIO</a> e acompanhei etapa por etapa para o desenvolvimento do projeto.</p>
---
+++ src/scripts/engine.js
@@ -0,0 +1,51 @@
+const emojis = [
+    "👻",
+    "👻",
+    "😸",
+    "😸",
+    "🐶",
+    "🐶",
+    "🐵",
+    "🐵",
+    "🦁",
+    "🦁",
+    "🦊",
+    "🦊",
+    "🦝",
+    "🦝",
+    "🐼",
+    "🐼"];
+let openCards = [];
+
+let randomEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0;i<emojis.length;i++){
+    let box = document.createElement("div");
+    box.className = ("item");
+    box.innerHTML = randomEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }else if(openCards.length == 2){
+        checkMatch();
+    }
+}
+
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você ganhou")
+    }
+}
